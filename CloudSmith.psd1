769c832ac6ba332cd432544791f5cb178a1a2b13
Stephen Valdinger <stephen@chocolatey.io>
2022-02-17 20:28:22 -0500

    Add storage region api support

--- CloudSmith.psd1
+++ CloudSmith.psd1
@@ -73,7 +73,8 @@ FunctionsToExport = @('Connect-CloudsmithInstance',
 'Get-CloudsmithRepository',
 'New-CloudsmithRepository',
 'Remove-CloudsmithRepository',
-'Set-CloudsmithRepository')
+'Set-CloudsmithRepository',
+'Get-CloudsmithRegion')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = '*'
---
+++ src/public/Regions/Get-CloudsmithRegion.ps1
@@ -0,0 +1,41 @@
+function Get-CloudsmithRegion {
+    <#
+    .SYNOPSIS
+    Get a storage region information from Cloudsmith
+    
+    .DESCRIPTION
+    Get storage region information from Cloudsmith
+    
+    .PARAMETER Slug
+    Get a specif storage region
+    
+    .EXAMPLE
+    Get-CloudsmithRegion
+
+    .EXAMPLE
+    Get-CloudsmithRegion -Slug 'us-ohio'
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [Alias('Name')]
+        $Slug
+    )
+
+    begin {
+        if(-not $header){
+            throw "Not connected to Cloudsmith API! Run Connect-CloudsmithInstance first!"
+        }
+
+    }
+    
+    process {
+        $endpoint = 'storage-regions/'
+
+        if($Slug){
+            $endpoint = 'storage-regions/' + "$Slug/"
+        }
+
+        Invoke-Cloudsmith -Endpoint $endpoint -Method 'GET'
+    }
+}
